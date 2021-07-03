15368df5590409ce9a6d4f0d08e6b2c392fe188d
jawsildc <86760096+jawsildc@users.noreply.github.com>
2021-07-03 12:21:16 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 
 include_once("signup.html")
-include_once("analyticstracking.php")
+//include_once("analyticstracking.php")
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C9E7HLLR8F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C9E7HLLR8F');
+</script>
 
 
 ?>
